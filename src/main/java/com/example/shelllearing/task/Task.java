b50a151d9af516cae8ecb4ca44bc8e48ac92fe4d
AidenDan <1299930560@qq.com>
2021-04-03 22:45:44 +0800

    格式化

--- src/main/java/com/example/shelllearing/task/Task.java
+++ src/main/java/com/example/shelllearing/task/Task.java
@@ -1,11 +1,10 @@
 package com.example.shelllearing.task;
 
-        import lombok.extern.slf4j.Slf4j;
-        import org.springframework.scheduling.annotation.Scheduled;
-        import org.springframework.stereotype.Component;
-
-        import java.time.LocalDateTime;
-        import java.time.format.DateTimeFormatter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author Aiden
@@ -19,8 +18,8 @@ package com.example.shelllearing.task;
 public class Task {
 
     // 定时任务 程序启动10秒后开始执行 每5秒执行一次
-    @Scheduled(initialDelay = 1000*10, fixedRate = 1000*5)
-    public void taskEchoData(){
+    @Scheduled(initialDelay = 1000 * 10, fixedRate = 1000 * 5)
+    public void taskEchoData() {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formatTime = formatter.format(now);
